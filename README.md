f117110c99e8d5bdf629cc3142a77d5e9dd0fd29
tylerhand1 <tyha7462@colorado.edu>
2023-06-22 14:52:15 -0600

    Created skeleton HTML/CSS/JS files

---
+++ README.md
@@ -0,0 +1,2 @@
+## Library
+A simple book library made with HTML, CSS and JavaScript.
---
+++ footer.js
@@ -0,0 +1,3 @@
+const footer = document.querySelector('.footer');
+
+footer.textContent = `Copyright © ${footer.textContent} ${(new Date()).getFullYear()} tylerhand1`;
