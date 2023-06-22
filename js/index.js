3f1fef3023fbf918cbd1de36adeced0c46774043
tylerhand1 <tyha7462@colorado.edu>
2023-06-22 16:32:29 -0600

    Added JS logic to create a form modal from w3schools example

--- js/index.js
+++ js/index.js
@@ -7,3 +7,21 @@ function Book() {
 function addToLibrary() {
     
 }
+
+const modal = document.querySelector('.modal');
+const closeBtn = document.querySelector('.close');
+const addBookBtn = document.querySelector('.btn-add');
+
+addBookBtn.addEventListener('click', () => {
+    modal.classList.add('active');
+});
+
+closeBtn.addEventListener('click', () => {
+    modal.classList.remove('active');
+})
+
+window.addEventListener('click', e => {
+    if(e.target === modal) {
+        modal.classList.remove('active');
+    }
+});
