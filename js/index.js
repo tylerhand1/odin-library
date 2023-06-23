a2d28920c42437d1246a11c1ca2466a984a4b934
tylerhand1 <tyha7462@colorado.edu>
2023-06-23 11:15:00 -0600

    Styled form modal

--- js/index.js
+++ js/index.js
@@ -12,22 +12,17 @@ function addToLibrary() {
 }
 
 function removeFromLibrary() {
-    
+
 }
 
 // Form modal logic
 
 const modal = document.querySelector('.modal');
-const closeBtn = document.querySelector('.close');
 const addBookBtn = document.querySelector('.btn-add');
 
 addBookBtn.addEventListener('click', () => {
     modal.classList.add('active');
 });
-
-closeBtn.addEventListener('click', () => {
-    modal.classList.remove('active');
-})
 
 window.addEventListener('click', e => {
     if(e.target === modal) {
